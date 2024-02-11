de191e7723ddce5b05de166daa5d94428e8f4a85
resu-xuniL <linux@lavache.com>
2024-02-11 18:11:25 +0100

    Added prompt for steps selection

--- run.sh
+++ run.sh
@@ -18,42 +18,48 @@ change_xfce_terminal_display
 
 if grep -q "ArcoLinux" /etc/os-release; then
 
-    # init
+    # choice
     header
     prompt_to_continue
-    display_step "Initialization"
-    sleep 1
-    step init               "Initialization"
-    prompt_to_continue
 
-    # Uninstall
-    display_step "Software uninstallation"
-    sleep 1
-    step uninstall_software "Software uninstallation"
-    prompt_to_continue
+    all_steps() {
+        # init
+        display_step "Initialization"
+        sleep 1
+        step init               "Initialization"
+        prompt_to_continue
 
-    # Update system
-    display_step "Updating system"
-    sleep 1
-    step update_system      "Updating system"
-    prompt_to_continue
+        # Uninstall
+        display_step "Software uninstallation"
+        sleep 1
+        step uninstall_software "Software uninstallation"
+        prompt_to_continue
 
-    # Install
-    display_step "Software installation"
-    sleep 1
-    step install_software   "Software installation"
-    prompt_to_continue
+        # Update system
+        display_step "Updating system"
+        sleep 1
+        step update_system      "Updating system"
+        prompt_to_continue
 
-    # Configuration
-    display_step "System configuration"
-    sleep 1
-    step config_files       "Files system configuration"
-    step config_settings    "System configuration"
-    prompt_to_continue
+        # Install
+        display_step "Software installation"
+        sleep 1
+        step install_software   "Software installation"
+        prompt_to_continue
 
-    # End
-    sleep 1
-    endscript "${start_time}"
+        # Configuration
+        display_step "System configuration"
+        sleep 1
+        step config_files       "Files system configuration"
+        step config_settings    "System configuration"
+        prompt_to_continue
+
+        # End
+        sleep 1
+        endscript "${start_time}"
+    }
+
+    choose_steps
 else
     exec_log "xfconf-query -c xfce4-terminal -p /background-darkness -s 0.85" "${GREEN}[+]${RESET} XFCE terminal : Restoring [${YELLOW}BACKGROUND DARKNESS${RESET}] to [${YELLOW}0.85${RESET}]"
     exec_log "xfconf-query -c xfce4-terminal -p /font-use-system -s true" "${GREEN}[+]${RESET} XFCE terminal : Restoring use of system [${YELLOW}FONT${RESET}] to [${YELLOW}TRUE${RESET}]"
