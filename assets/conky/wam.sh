4ed27eb4715d6de445c2efdb3c087f329b2162ec
resu-xuniL <linux@lavache.com>
2024-03-07 19:08:23 +0100

    Conky : cosmetic change (icons for temperature)

--- assets/conky/wam.sh
+++ assets/conky/wam.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-weather_icon() {
+weather_icons() {
     local icons_path=~/.config/conky/images/meteo-icons
     local icon_name=$(jq .current.weather_code ~/.cache/openmeteo/weather.json)
+    local temperature=$(printf '%.*f' 0 $(jq .current.temperature_2m ~/.cache/openmeteo/weather.json))
 
     if [[ $(date +"%H") > 07 && $(date +"%H") < 22 ]]; then
         cp -f ${icons_path}/${icon_name}.png ~/.cache/openmeteo/current.png
@@ -11,6 +12,12 @@ weather_icon() {
     else
         cp -f ${icons_path}/${icon_name}n.png ~/.cache/openmeteo/current.png
     fi
+
+    if [[ ${temperature} -ge 17 ]]; then
+        cp -f ${icons_path}/900.png ~/.cache/openmeteo/therm.png
+    else
+        cp -f ${icons_path}/901.png ~/.cache/openmeteo/therm.png
+    fi
 }
 
-weather_icon
+weather_icons
